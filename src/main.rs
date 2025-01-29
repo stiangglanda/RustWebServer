baff3932981618a920709f8fb262b9f848583f5d
stiangglanda <leander.kieweg@gmail.com>
2025-01-29 10:51:08 +0100

    the web server is now able to send html files and some basic error handling with a 404 page

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::{
-    io::{prelude::*, BufReader },
-    net::{TcpListener, TcpStream },
-};
+use std::{fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread, time::Duration};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    println!("ip: 127.0.0.1:7878");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -15,6 +14,30 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_>=buf_reader.lines().map(|result| result.unwrap()).take_while(|line|!line.is_empty()).collect();
-    println!("Request: {:#?}", http_request);
+    let request_line = buf_reader
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap();
+
+    //let http_request: Vec<_>=buf_reader.lines().map(|result| result.unwrap()).take_while(|line|!line.is_empty()).collect();
+    let (status_line,filename)=match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents=fs::read_to_string(filename).unwrap();
+    let length=contents.len();
+
+    let response = format!(
+        "{status_line}{filename}\r\n\
+        Content-Length: {length}\r\n\r\n\
+        {contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
